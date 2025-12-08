7ef28671fed318c120260741bdf8eba80796c0dc
August Cornfeld <august.cornfeld@thelangschool.net>
2025-12-08 11:28:32 -0500

    did some stuff

--- dataToUsers.py
+++ dataToUsers.py
@@ -1,6 +1,13 @@
 def dataToUsers(strings):
-
+    users=[]
     for s in strings:
-        u:User=none
-        u.firstName=s.split(" ")[0]
-        u.firstName=s.split(" ")[1]
+        for user in users:
+            if s.split(" ")[0]==user.firstName and s.split(" ")[1]==user.lastName:
+                if s.split(" ")[3]=="added":
+                    //do some stuff
+                if s.split(" ")[3]=="posted":
+                    b=s.split(" ")
+                    del b[:3]
+                    u.posts.append("".join(b))
+                if s.split(" ")[3]=="liked":
+                    //do some stuff
