9928cb17b3303fc12c4cb3876ea9258125889511
Jim Stewart <jim.stewart@thelangschool.net>
2026-01-06 09:22:17 -0500

    fixed and put into a function

--- FriendTracker.py
+++ FriendTracker.py
@@ -1,16 +1,19 @@
-f = open("log.txt", "r")
-lines = f.readlines()
-friends = []
-for line in lines:
-    if "as a friend" in line:
-        friends.append(line.strip())
-print(friends)
-name = ""
-for string in friends:
-    words = string.split()
-    for word in words:
-        if word != "added":
-            name = name + word + " "
-        else:
-            break
-print(name)
+ def get_friends():
+    f = open("log.txt", "r")
+    lines = f.readlines()
+    friendlines = []
+    friends = []
+    for line in lines:
+        if "as a friend" in line:
+            friendlines.append(line.strip())
+    for string in friendlines:
+        name = ""
+        words = string.split()
+        for word in words:
+            if word != "added":
+                name = name + word + " "
+            else:
+                break
+        if name not in friends:
+            friends.append(name)
+    return friends
