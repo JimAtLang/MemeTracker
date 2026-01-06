22f2fc8fc111ef1ad4daf705c7eff0cdff2e24e3
Jim Stewart <jim.stewart@thelangschool.net>
2026-01-06 09:03:09 -0500

    added User class

---
+++ User.py
@@ -0,0 +1,12 @@
+class User:
+  def __init__(
+  self,
+  username,
+  friends,
+  likes,
+  postsnum
+  ):
+    self.username = username,
+    self.friends = friends,
+    self.likes = likes,
+    self.postsnum = postsnum
