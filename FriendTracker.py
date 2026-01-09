a74031909f804f966ef587ec068e2469578b1117
9M 01 <9m01@thelangschool.net>
2026-01-09 12:21:20 -0500

    get name pairs

--- FriendTracker.py
+++ FriendTracker.py
@@ -2,18 +2,27 @@ def get_friends():
     f = open("log.txt", "r")
     lines = f.readlines()
     friendlines = []
-    friends = []
+    friends = {}
     for line in lines:
         if "as a friend" in line:
             friendlines.append(line.strip())
     for string in friendlines:
-        name = ""
+        name1 = ""
         words = string.split()
+        #this for loop gets the first name
+        wordnumber = 0
         for word in words:
+            wordnumber += 1
             if word != "added":
-                name = name + word + " "
+                name1 = name1 + word + " "
             else:
                 break
-        if name not in friends:
-            friends.append(name)
+        word = words[wordnumber]
+        name2 = ""
+        while word != "as":
+            name2 += word + " "
+            wordnumber += 1
+            word = words[wordnumber]
+        # now we need to make a dictionary
+        print(name1, name2)
     return friends
